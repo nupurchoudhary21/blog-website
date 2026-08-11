e146c2c77ceb8a1bd3233c97d3ddea63ba34f872
Nupur <nupurchoudhary005@gmail.com>
2026-08-11 20:43:01 +0530

    deploying website

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -32,7 +32,7 @@ export default function Navbar() {
   return (
     <nav className="fixed top-0 left-0 w-full  bg-[#C96868] shadow-md z-50 ">
       <div className="flex items-center justify-left h-16 px-4 py-4 space-x-8 text-white text-lg">
-        <img className="h-10 w-13 rounded-lg mx-2" src="/public/logo.png" />
+        <img className="h-10 w-13 rounded-lg mx-2" src="/logo.png" />
         <div className="text-2xl font-bold">Blogify </div>
         <Link to="/">Home</Link>
 
